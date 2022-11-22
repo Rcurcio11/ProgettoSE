0858b08b39cd67943b1aa0b36ff9ead3e684dc2e
Rosarii00 <rosariocurcio1012@gmail.com>
2022-11-22 20:33:47 +0100

    Added JavaFX
    This is an empty JavaFX project, necessary after the decision to move from Java Swing to JavaFX.

---
+++ src/seproject/FXMLGuiDocumentController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package seproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Rosario
+ */
+public class FXMLGuiDocumentController implements Initializable {
+
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    
+}
