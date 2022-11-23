749f3f829cb16f4595dd72243eea2d0840f43d79
giuseppefusco <giuseppefusco@mbpdigiuseppe3.station>
2022-11-23 11:57:03 +0100

    CREATE EMPTY WINDOW_4

---
+++ src/seproject/DrawShape.java
@@ -0,0 +1,22 @@
+
+package seproject;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Group14
+ */
+public abstract class DrawShape {
+    
+    public void insert (AnchorPane drawingPane, double prevX, double prevY, double lastX, double lastY){
+        
+    }
+    public void nextDraw (){
+        
+    }
+    public void setColor (Color outlineColor, Color fillingColor){
+        
+    }
+}
