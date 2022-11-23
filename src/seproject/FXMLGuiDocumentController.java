ece966ca9bb14d1e642b18f1ee7cf247689f620b
Rosarii00 <rosariocurcio1012@gmail.com>
2022-11-23 12:39:15 +0100

    SELECT SHAPE POSITION_1

--- src/seproject/FXMLGuiDocumentController.java
+++ src/seproject/FXMLGuiDocumentController.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 
@@ -19,12 +21,24 @@ public class FXMLGuiDocumentController implements Initializable {
     @FXML
     private HBox toolBox;
 
-    
+    private Point2D startPoint;
+    private Point2D endPoint;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        startPoint = new Point2D(0,0);
+        endPoint = new Point2D(0,0);
+    }
+
+    @FXML
+    private void endDrawingPoint(MouseEvent event) {
+        endPoint = new Point2D(event.getX(),event.getY());
+    }
+
+    @FXML
+    private void startDrawingPoint(MouseEvent event) {
+        startPoint = new Point2D(event.getX(),event.getY());
+    }
 
     
 }
