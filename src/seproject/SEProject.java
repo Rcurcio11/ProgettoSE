b76e9dc76a45322e9a424f9f98a8300b7ab42215
Gaia Foroni <uondi@mbp-di-gaia.station>
2022-11-23 18:10:12 +0100

    add class ShapeRectangle and hamcrest library

--- src/seproject/SEProject.java
+++ src/seproject/SEProject.java
@@ -18,7 +18,6 @@ public class SEProject extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLGuiDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/seproject/ShapeRectangle.java
@@ -0,0 +1,54 @@
+
+package seproject;
+
+import static java.lang.Math.abs;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+
+/**
+ *
+ * @author Group14
+ */
+public class ShapeRectangle extends DrawShape{
+    private Rectangle rectangle;
+
+    public ShapeRectangle() {
+        rectangle = new Rectangle();
+    }
+
+    @Override
+    public void nextDraw() {
+        rectangle = new Rectangle();
+    }
+
+    @Override
+    public void insert(AnchorPane drawingPane, double prevX, double prevY, double lastX, double lastY) {
+        if(prevX>lastX && prevY>lastY){
+            rectangle.setX(lastX);
+            rectangle.setY(lastY);
+            
+        }else if(prevX>lastX){
+
+            rectangle.setX(lastX);
+            rectangle.setY(prevY);
+            
+        }else if(prevY>lastY){
+
+            rectangle.setX(prevX);
+            rectangle.setY(lastY);
+
+        }else{
+
+            rectangle.setX(prevX);
+            rectangle.setY(prevY);
+
+        }
+        double width = abs(lastX-prevX);
+        double height = abs(lastY- prevY);
+        
+        rectangle.setWidth(width);
+        rectangle.setHeight(height);
+        
+        drawingPane.getChildren().add(rectangle);
+    }
+}
